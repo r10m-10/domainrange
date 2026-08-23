df37f0007dae518014a1ef8a35733400c9590f7b
R10M <reyaanshsharma11@gmail.com>
2026-08-23 18:05:28 +0530

    svg tree download feature

--- static/js/ast_visualizer.js
+++ static/js/ast_visualizer.js
@@ -3,6 +3,17 @@ setupExpressionInput('/api/ast-visualize', (data, resultDiv) => {
 
   const downloadBtn = document.getElementById('download-svg-btn')
   downloadBtn.style.display = 'flex'
+  downloadBtn.onclick = () => {
+    const svgEl = resultDiv.querySelector('svg')
+    if (!svgEl) return
+    const blob = new Blob([svgEl.outerHTML], { type : 'image/svg+xml' })
+    const url = URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.href = url
+    a.download = 'ast-tree.svg'
+    a.click()
+    URL.revokeObjectURL(url)
+  }
 });
 setupToolNav('ast-visualizer')
 setupTestButton('3x^2 + sin(x) - 5')
