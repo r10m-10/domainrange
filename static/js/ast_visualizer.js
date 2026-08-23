8dcd83c4a8eff7e5cb4e8a10449134a020dd1ec0
R10M <reyaanshsharma11@gmail.com>
2026-08-23 17:21:07 +0530

    favicons

--- static/js/ast_visualizer.js
+++ static/js/ast_visualizer.js
@@ -1,5 +1,8 @@
 setupExpressionInput('/api/ast-visualize', (data, resultDiv) => {
   resultDiv.innerHTML = data.svg;
+
+  const downloadBtn = document.getElementById('download-svg-btn')
+  downloadBtn.style.display = 'flex'
 });
 setupToolNav('ast-visualizer')
 setupTestButton('3x^2 + sin(x) - 5')
