f249b760c74dc2338d5d4e2650109e70932fe250
R10M <reyaanshsharma11@gmail.com>
2026-08-25 23:45:16 +0530

    completed need_parens

--- engine/ast/unparse.py
+++ engine/ast/unparse.py
@@ -2,6 +2,33 @@ from .tokenizer import tokenize
 from .parser import parse_add
 from ..calculators.simplify import simplify, pow_to_div
 
+precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
+
+def needs_paren(child, op, side):
+    if type(child) == str:
+        return False
+    elif len(child) == 2:
+        return False
+    elif len(child) == 3:
+        child_op = child[0]
+        if precedence[child_op] < precedence[op]:
+            return True
+        elif precedence[child_op] > precedence[op]:
+            return False
+        elif precedence[child_op] == precedence[op]:
+            if op in '+*':
+                return False
+            elif op in '-/':
+                if side == 'right':
+                    return True
+                else:
+                    return False
+            elif op == '^':
+                if side == 'left':
+                    return True
+                else:
+                    return False
+
 def unparse(node):
     if type(node) == str:
         return node
@@ -11,25 +38,16 @@ def unparse(node):
         right_unparsed = unparse(right)
 
         if op in '+-':
-            return f"{left} {op} {right}"
+            return f"{left_unparsed} {op} {right_unparsed}"
         elif op in '*/':
-            if type(left) == type(right) == tuple and len(left) == len(right) == 3 and (left[0] in '+-' and right[0] in '+-') or op == '/':
+            if type(left) == type(right) == tuple and len(left) == len(right) == 3 and ((left[0] in '+-' and right[0] in '+-') or op == '/'):
                 return f"({left_unparsed}) {op} ({right_unparsed})"
-            elif (type(left) == type(right) == tuple and len(left) == 3 and len(right) == 2) or (type(left) == tuple and left[0] in '+-') :
+            elif (type(left) == type(right) == tuple and len(left) == 3 and len(right) == 2) or (type(left) == tuple and left[0] in '+-') or (op == '/' and type(left) == tuple and len(left) == 3):
                 return f"({left_unparsed}) {op} {right_unparsed}"
-            elif (type(left) == type(right) == tuple and right[0] in '+-' and right[0] == '*') or (type(right) == tuple and right[0] in '+-'):
+            elif (type(left) == type(right) == tuple and len(left) == 2 and len(right) == 3) or (type(right) == tuple and right[0] in '+-') or (op == '/' and type(right) == tuple and len(right) == 3):
                 return f"{left_unparsed} {op} ({right_unparsed})"
-            elif op == '*' and type(left) == tuple and type(right) == str and left[0] == '*':
+            elif (type(left) == type(right) == tuple and len(left) == len(right) == 2) or (type(left) == type(right) == str) or (type(left) == str and (type(right) == tuple and len(right) == 2)) or (type(right) == str and (type(left) == tuple and len(left) == 2)) or (op == '*' and type(left) == tuple and left[0] == '*' and (type(right) == str or (type(right) == tuple and len(right) == 2))):
                 return f"{left_unparsed} {op} {right_unparsed}"
-            else:
-                print(left)
-                if left.isalpha():
-                    return f"{right_unparsed}{left_unparsed}"
-                else:
-                    return f"{left_unparsed}{right_unparsed}"
-        elif op == '/':
-            if type(left) == type(right) == tuple and (left[0] in '+-' and right[0] in '+-'):
-                return f"({left_unparsed}) {op} ({right_unparsed})"
 
 def get_node(exp):
     tokens = tokenize(exp)
@@ -37,7 +55,6 @@ def get_node(exp):
     sim = pow_to_div(simplify(node))
     return sim
 
-# test -> '(x+2)/(x/(x+1))', '(x+2)/(x+3) * (x+1)/(x+4)'
-node = get_node('(x+1) * 3 * x')
+node = get_node('(x+2)/(x+3) * (x+1)/(x+4)')
 print(node)
 print(unparse(node))
